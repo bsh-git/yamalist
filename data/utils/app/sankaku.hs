0a8f9b2c7474d995729887e750398fd33c799604
Hiroyuki Bessho <bsh@grotto.jp>
2024-03-16 01:13:24 +0900

    cleanup

--- data/utils/app/sankaku.hs
+++ data/utils/app/sankaku.hs
@@ -3,11 +3,12 @@
 --
 module Main where
 
-import Debug.Trace
-
 import Data.List
 import System.Environment
 import Text.XML.Light
+
+--import Debug.Trace
+
 
 main :: IO ()
 main =
@@ -64,8 +65,6 @@ makePlacemark mark =
 
         _ -> p
 
----        x -> trace ("not name" ++ show x) p
-
     getCoord :: Element -> (Double, Double)
     getCoord (Element { elContent = c }) =
       let CData {cdData = s} = (onlyText c) !! 0
@@ -73,32 +72,6 @@ makePlacemark mark =
       in
         (read lat, read lon)
         
-{-
-Element {elName = QName {qName = "Placemark", qURI = Just "http://www.opengis.net/kml/2.2", qPrefix = Nothing},
-         elAttribs = [],
-         elContent = [Elem (Element {elName = QName {qName = "name", qURI = Just "http://www.opengis.net/kml/2.2", qPrefix = Nothing},
-                                     elAttribs = [],
-                                     elContent = [Text (CData {cdVerbatim = CDataText, cdData = "TR15439010501", cdLine = Just 26})],
-                                     elLine = Just 26}),
-                      Elem (Element {elName = QName {qName = "styleUrl", qURI = Just "http://www.opengis.net/kml/2.2", qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "#t1p", cdLine = Just 26})], elLine = Just 26}),
-                      Elem (Element {elName = QName {qName = "Point", qURI = Just "http://www.opengis.net/kml/2.2", qPrefix = Nothing},
-                                     elAttribs = [],
-                                     elContent = [Elem (Element {elName = QName {qName = "coordinates", qURI = Just "http://www.opengis.net/kml/2.2", qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "139.1900705,36.00575167", cdLine = Just 26})], elLine = Just 26})], elLine = Just 26}),
-                      Elem (Element {
-                               elName = QName {qName = "description",
-                                                qURI = Just "http://www.opengis.net/kml/2.2",
-                                                qPrefix = Nothing},
-                                 elAttribs = [],
-                                 elContent = [Text (CData {cdVerbatim = CDataVerbatim, cdData = "  <div style=\"font-size:1em;\"><table border=1 cellspacing=0><tr><th>\19968\31561\19977\35282\28857</th><th>\22530\24179\23665</th></tr><tr><th></th><th>875.91 m</th></tr><tr><th colspan=2>36.00575167 139.1900705</th></tr><tr><th>2014-04-01</th><th>2999-12-31</th></tr></table></div>  ", cdLine = Just 26})],
-                                      elLine = Just 26})],
-         elLine = Just 26}
-
-[Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\22522\28310\28857\12467\12540\12489", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "TR15439055601", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),
- Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\31561\32026\31278\21029", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "TR1", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),
- Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\22522\28310\28857\21517", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\39640\37326\26449", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\12527\12531\12473\12488\12483\12503\21487\21542", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\9675", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\25104\26524\29366\24907", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\27491\24120", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\21271\32239", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "36\176\&02\8242\&45\8243.3633", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\26481\32076", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "139\176\&42\8242\&01\8243.5379", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\27161\39640", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "10.15", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\29694\27841\29366\24907", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\27491\24120", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "tr", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "\24180\26376\26085", cdLine = Just 1})], elLine = Just 1}),Elem (Element {elName = QName {qName = "td", qURI = Nothing, qPrefix = Nothing}, elAttribs = [], elContent = [Text (CData {cdVerbatim = CDataText, cdData = "20110622", cdLine = Just 1})], elLine = Just 1})], elLine = Just 1})]
--}
-
-
 --
 -- Local Variables:
 -- coding: utf-8
